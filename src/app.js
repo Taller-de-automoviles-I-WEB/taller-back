0c311207bf578559e5b7d72ce5d0663dec0a8473
MatíasJMaranga <marangamatias@gmail.com>
2023-09-13 20:26:42 -0300

    app_helmet

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const handleLog = require('../src/utils/handle.logs');
 const cors = require('cors');
 const routes = require('./routes/index.routes');
+const helmet = require('helmet');
 
 const app = express();
 
 app.name = "TALLER AUTOS API"
 
-
+app.use(helmet());
+app.use(handleLog.morganLogger);
 app.use(express.json());
 app.use(cors());
-app.use(handleLog.morganLogger);
 app.use('/', routes);
+
 module.exports = app;
