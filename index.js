8176827beffa1a81cdefff96ccf6a6c97c655f23
cahov <christian.herrera.onaindi@gmail.com>
2023-09-16 18:02:09 -0400

    refactor: refactoring .env import

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
+const db = require('./src/database/models');
 const app = require('./src/app');
 const {PORT} = process.env;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server running in port ${PORT}`)
+    try {
+        await db.sequelize.authenticate();
+        console.log('Database connect!');
+    } catch (error) {
+        console.log(error)
+    }
 })
