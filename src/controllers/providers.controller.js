31a92c957f6ca652392383202212a0dec28fcab7
cahov <christian.herrera.onaindi@gmail.com>
2023-09-18 20:21:40 -0400

    chore: update routes to use /api prefix

--- src/controllers/providers.controller.js
+++ src/controllers/providers.controller.js
@@ -1,107 +1,129 @@
-const {getProvidersService, createProviderService, getProviderByEmailService, getProviderByIdService, updateProviderService, deleteProviderByIdService, deleteAllProvidersService} = require('../services/provider.services')
-const handleHttp = require('../utils/handle.logs')
+const {
+  getProvidersService,
+  createProviderService,
+  getProviderByEmailService,
+  getProviderByIdService,
+  updateProviderService,
+  deleteProviderByIdService,
+  deleteAllProvidersService,
+} = require("../services/provider.services");
+const handleHttp = require("../utils/handle.logs");
 
-const getProviders = async (req, res) =>{
-    try {
-        const providers = await getProvidersService()
-        if(providers?.length > 0 ){
-            res.status(200).json({ providers, message: 'Providers success found.' });
-        }else{
-            return res.status(200).json({ message: 'No providers found.' });
-        }
-    } catch (error) {
-        handleHttp.errorHandler(error);
-        throw error
+const getProviders = async (req, res) => {
+  try {
+    const providers = await getProvidersService();
+    if (providers?.length > 0) {
+      res.status(200).json({ providers, message: "Providers success found." });
+    } else {
+      return res.status(200).json({ message: "No providers found." });
     }
-}
-
-const getProviderByEmail = async (req, res) => {
-    try {
-        const provider = await getProviderByEmailService(req.params.email)
-        if(provider){
-            res.status(200).json({ provider, message: 'Provider success found.' });
-        }else{
-            return res.status(200).json({ message: 'Provider no found.' });
-        }
-    } catch (error) {
-        throw error
-    }
-}
-
-const getProviderById = async (req, res) => {
-    try {
-        const provider = await getProviderByIdService(parseInt(req.params.id))
-        if(provider){
-            res.status(200).json({ provider, message: 'Provider success found.' });
-        }else{
-            return res.status(200).json({ message: 'Provider no found.' });
-        }
-    } catch (error) {
-        throw error
-    }
-}
-
-
-const createProvider = async (req, res) =>{
-    try {
-        const newProvider = await createProviderService(req.body)
-        res.status(200).json({ newProvider, message: 'Provider create success' });
-    } catch (error) {
-        handleHttp.errorHandler(error);
-        if(error.name === 'SequelizeUniqueConstraintError'){
-            res.status(409).json({message: 'Email already in use.' });
-        }else{
-            throw error
-        }
-    }
-}
-
-const updateProvider = async (req, res) =>{
-    try {
-        const newProvider = await updateProviderService(parseInt(req.params.id), req.body)
-        res.status(200).json({ newProvider, message: 'Provider update success' });
-    } catch (error) {
-        handleHttp.errorHandler(error);
-        if (error.name === 'SequelizeUniqueConstraintError') {
-            res.status(409).json({ message: 'Email already in use.' });
-        } else if (error.message === 'Provider not found') {
-            res.status(404).json({ message: 'Provider not found.' });
-        } else {
-            console.log(error);
-            res.status(500).json({ message: 'Internal server error' });
-        }
-    }
-}
-
-const deletedProviderById = async (req, res) => {
-    try {
-        const deletedCount = await deleteProviderByIdService(parseInt(req.params.id));
-        if (deletedCount === 1) {
-            res.status(200).json({ message: 'Provider successfully deleted.' });
-        }
-    } catch (error) {
-        if (error.message === 'Provider not found') {
-            res.status(404).json({ message: 'Provider not found.' });
-        } else {
-            res.status(500).json({ message: 'Internal server error' });
-            console.log(error)
-        }
-    }
-}
-
-const deleteAllProviders = async (req, res) => {
-    try {
-        const deletedCount = await deleteAllProvidersService();
-
-        if (deletedCount > 0) {
-            res.status(200).json({ message: `${deletedCount} providers successfully deleted.` });
-        } else {
-            res.status(200).json({ message: 'No providers found to delete.' });
-        }
-    } catch (error) {
-        handleHttp.errorHandler(error);
-        throw error
-    }
+  } catch (error) {
+    handleHttp.errorHandler(error);
+    throw error;
+  }
 };
 
-module.exports = {getProviders, createProvider, getProviderByEmail, getProviderById, updateProvider, deletedProviderById, deleteAllProviders}
+const getProviderByEmail = async (req, res) => {
+  try {
+    const provider = await getProviderByEmailService(req.params.email);
+    if (provider) {
+      res.status(200).json({ provider, message: "Provider success found." });
+    } else {
+      return res.status(200).json({ message: "Provider no found." });
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getProviderById = async (req, res) => {
+  try {
+    const provider = await getProviderByIdService(parseInt(req.params.id));
+    if (provider) {
+      res.status(200).json({ provider, message: "Provider success found." });
+    } else {
+      return res.status(200).json({ message: "Provider no found." });
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const createProvider = async (req, res) => {
+  try {
+    const newProvider = await createProviderService(req.body);
+    res.status(200).json({ newProvider, message: "Provider create success" });
+  } catch (error) {
+    handleHttp.errorHandler(error);
+    if (error.name === "SequelizeUniqueConstraintError") {
+      res.status(409).json({ message: "Email already in use." });
+    } else {
+      throw error;
+    }
+  }
+};
+
+const updateProvider = async (req, res) => {
+  try {
+    const newProvider = await updateProviderService(
+      parseInt(req.params.id),
+      req.body
+    );
+    res.status(200).json({ newProvider, message: "Provider update success" });
+  } catch (error) {
+    handleHttp.errorHandler(error);
+    if (error.name === "SequelizeUniqueConstraintError") {
+      res.status(409).json({ message: "Email already in use." });
+    } else if (error.message === "Provider not found") {
+      res.status(404).json({ message: "Provider not found." });
+    } else {
+      console.log(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+};
+
+const deletedProviderById = async (req, res) => {
+  try {
+    const deletedCount = await deleteProviderByIdService(
+      parseInt(req.params.id)
+    );
+    if (deletedCount === 1) {
+      res.status(200).json({ message: "Provider successfully deleted." });
+    }
+  } catch (error) {
+    if (error.message === "Provider not found") {
+      res.status(404).json({ message: "Provider not found." });
+    } else {
+      res.status(500).json({ message: "Internal server error" });
+      console.log(error);
+    }
+  }
+};
+
+const deleteAllProviders = async (req, res) => {
+  try {
+    const deletedCount = await deleteAllProvidersService();
+
+    if (deletedCount > 0) {
+      res
+        .status(200)
+        .json({ message: `${deletedCount} providers successfully deleted.` });
+    } else {
+      res.status(200).json({ message: "No providers found to delete." });
+    }
+  } catch (error) {
+    handleHttp.errorHandler(error);
+    throw error;
+  }
+};
+
+module.exports = {
+  getProviders,
+  createProvider,
+  getProviderByEmail,
+  getProviderById,
+  updateProvider,
+  deletedProviderById,
+  deleteAllProviders,
+};
--- src/services/provider.services.js
+++ src/services/provider.services.js
@@ -1,123 +1,139 @@
-const db = require('../database/models');
-const passwordHandler = require('../utils/handle.password.encrypt')
-const handleHttp = require('../utils/handle.logs')
+const db = require("../database/models");
+const passwordHandler = require("../utils/handle.password.encrypt");
+const handleHttp = require("../utils/handle.logs");
 
-const getProvidersService = async () =>{
-    try {
-        const providers = await db.Provider.findAll()
-        return providers
-    } catch (error) {
-        handleHttp.errorHandler(error);
-        throw error
-    }
-}
-
-const getProviderByEmailService = async (email) => {
-    try {
-        const provider = await db.Provider.findOne({
-            where: {
-              email: email 
-            },
-            attributes: {
-                exclude: ['password']
-              }
-          });
-        return provider
-    } catch (error) {
-        handleHttp.errorHandler(error);
-        throw error
-    }
-}
-
-const getProviderByIdService = async (id) => {
-    try {
-        const provider = await db.Provider.findOne({
-            where: {
-              id: id 
-            }
-          });
-        return provider
-    } catch (error) {
-        handleHttp.errorHandler(error);
-        throw error
-    }
-}
-
-const createProviderService = async (body) =>{
-    const t = await db.sequelize.transaction(); 
-    try {
-        const newProvider = body
-        newProvider.password = await passwordHandler.hashPassword(newProvider.password)
-        const createProvider = await db.Provider.create(newProvider, { transaction: t})
-        await t.commit();
-        return createProvider
-    } catch (error) {
-        await t.rollback();
-        handleHttp.errorHandler(error);
-        throw error
-    }
-}
-
-const updateProviderService = async (id, updatedData) => {
-    const t = await db.sequelize.transaction();
-    try {
-      const existingProvider = await db.Provider.findByPk(id);
-      if (!existingProvider) {
-        throw new Error('Provider not found');
-      }
-      if(updatedData.password){
-        updatedData.password = await passwordHandler.hashPassword(updatedData.password)
-      }
-      const updatedProvider = await existingProvider.update(updatedData, {
-        transaction: t
-      });
-  
-      await t.commit();
-      return updatedProvider;
-    } catch (error) {
-      await t.rollback();
-      handleHttp.errorHandler(error);
-      throw error;
-    }
-  };
-
-  const deleteProviderByIdService = async (id) =>{
-    const t = await db.sequelize.transaction();
-    try {
-        const existingProvider = await db.Provider.findByPk(id);
-        if(!existingProvider){
-            throw new Error('Provider not found');
-        }
-        const deletedCount = await db.Provider.destroy({
-            where: {
-              id: id,
-            },
-          }, { transaction: t});
-          await t.commit();
-          return deletedCount
-    } catch (error) {
-        await t.rollback();
-        handleHttp.errorHandler(error);
-        throw error;
-    }
+const getProvidersService = async () => {
+  try {
+    const providers = await db.Provider.findAll();
+    return providers;
+  } catch (error) {
+    handleHttp.errorHandler(error);
+    throw error;
   }
-
-  const deleteAllProvidersService = async () => {
-    const t = await db.sequelize.transaction();
-
-    try {
-        const deletedCount = await db.Provider.destroy({
-            where: {},
-            transaction: t,
-        });
-        await t.commit();
-        return deletedCount;
-    } catch (error) {
-        await t.rollback();
-        handleHttp.errorHandler(error);
-        throw error;
-    }
 };
 
-module.exports = {getProvidersService, createProviderService, getProviderByEmailService, getProviderByIdService, updateProviderService, deleteProviderByIdService, deleteAllProvidersService}
+const getProviderByEmailService = async (email) => {
+  try {
+    const provider = await db.Provider.findOne({
+      where: {
+        email: email,
+      },
+      attributes: {
+        exclude: ["password"],
+      },
+    });
+    return provider;
+  } catch (error) {
+    handleHttp.errorHandler(error);
+    throw error;
+  }
+};
 
+const getProviderByIdService = async (id) => {
+  try {
+    const provider = await db.Provider.findOne({
+      where: {
+        id: id,
+      },
+    });
+    return provider;
+  } catch (error) {
+    handleHttp.errorHandler(error);
+    throw error;
+  }
+};
+
+const createProviderService = async (body) => {
+  const t = await db.sequelize.transaction();
+  try {
+    const newProvider = body;
+    newProvider.password = await passwordHandler.hashPassword(
+      newProvider.password
+    );
+    const createProvider = await db.Provider.create(newProvider, {
+      transaction: t,
+    });
+    await t.commit();
+    return createProvider;
+  } catch (error) {
+    await t.rollback();
+    handleHttp.errorHandler(error);
+    throw error;
+  }
+};
+
+const updateProviderService = async (id, updatedData) => {
+  const t = await db.sequelize.transaction();
+  try {
+    const existingProvider = await db.Provider.findByPk(id);
+    if (!existingProvider) {
+      throw new Error("Provider not found");
+    }
+    if (updatedData.password) {
+      updatedData.password = await passwordHandler.hashPassword(
+        updatedData.password
+      );
+    }
+    const updatedProvider = await existingProvider.update(updatedData, {
+      transaction: t,
+    });
+
+    await t.commit();
+    return updatedProvider;
+  } catch (error) {
+    await t.rollback();
+    handleHttp.errorHandler(error);
+    throw error;
+  }
+};
+
+const deleteProviderByIdService = async (id) => {
+  const t = await db.sequelize.transaction();
+  try {
+    const existingProvider = await db.Provider.findByPk(id);
+    if (!existingProvider) {
+      throw new Error("Provider not found");
+    }
+    const deletedCount = await db.Provider.destroy(
+      {
+        where: {
+          id: id,
+        },
+      },
+      { transaction: t }
+    );
+    await t.commit();
+    return deletedCount;
+  } catch (error) {
+    await t.rollback();
+    handleHttp.errorHandler(error);
+    throw error;
+  }
+};
+
+const deleteAllProvidersService = async () => {
+  const t = await db.sequelize.transaction();
+
+  try {
+    const deletedCount = await db.Provider.destroy({
+      where: {},
+      transaction: t,
+    });
+    await t.commit();
+    return deletedCount;
+  } catch (error) {
+    await t.rollback();
+    handleHttp.errorHandler(error);
+    throw error;
+  }
+};
+
+module.exports = {
+  getProvidersService,
+  createProviderService,
+  getProviderByEmailService,
+  getProviderByIdService,
+  updateProviderService,
+  deleteProviderByIdService,
+  deleteAllProvidersService,
+};
